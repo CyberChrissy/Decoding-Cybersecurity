71236564ad02f36d0b0755481ee0c97bb0f253d1
Christiana Sanchez <christiana-sanchez@outlook.com>
2026-08-15 10:01:19 +0100

    Initial Python script exploring basics and security
    This script introduces basic Python concepts such as printing, variables, and data types. It also includes a simple security profile with user input for username and password.

---
+++ Initial exploration of Python.py
@@ -0,0 +1,29 @@
+# This is my first ever Python script
+print("Hello Cybersecurity world")
+print("My name is Christiana")
+print("I am learning python")
+print("I am also learning cybersecurity")
+# Exploring variables in Python
+name = "Christiana"
+age = 25
+height = 5.6
+is_student = True
+print("my name is", name, "I am", age, "years old", "I am", height, "tall")
+# Exploring data types in Python 
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+# Security profile
+username = "CyberChrissy"
+password = "SecurePass123"
+failed_attempts = 3
+Account_locked = False
+risk_score = 7.5
+print(type(username))
+print(type(password))
+print(type(failed_attempts))
+print(type(Account_locked))
+print(type(risk_score))
+input_username = input("Enter your username: ")
+input_password = input("Enter your password: ") 
