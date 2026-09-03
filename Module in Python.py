1ac6f497d88f5ad5c494bdb5d8de3d549fc54ef7
Christiana Sanchez <christiana-sanchez@outlook.com>
2026-09-03 08:48:58 +0100

    Create Python module with math, random, and datetime

---
+++ Module in Python.py
@@ -0,0 +1,10 @@
+# A module is a Python file containing code that can be imported and reused.
+import math
+print(math.sqrt(36))
+
+import random
+number = random.randint(1,50)
+print(number)
+
+import datetime
+print(datetime.datetime.now())
